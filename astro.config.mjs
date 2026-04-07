f334b9de84bba3b24d52704ebd48209bc3a6ee6f
Martin Cermak <martin@deltagreen.cz>
2026-04-07 14:34:31 +0200

    use Github Pages for preview

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-// TODO: nastavit před deplojem na GitHub Pages:
-//   site: 'https://YOUR_GITHUB_USERNAME.github.io'
-//   base: '/YOUR_REPO_NAME'   ← odstraň pokud máš custom doménu
-
 export default defineConfig({
-  // site: 'https://YOUR_GITHUB_USERNAME.github.io',
-  // base: '/YOUR_REPO_NAME',
+  site: 'https://martincermak.github.io',
+  base: '/mensedit',
   output: 'static',
   i18n: {
     defaultLocale: 'cs',
